2b0755ad166f342d91581fd460fb5071f29fe708
John <johncheung1997@gmail.com>
2021-03-18 04:14:57 -0400

    changed root component of entry.jsx

--- frontend/entry.jsx
+++ frontend/entry.jsx
@@ -3,5 +3,10 @@ import ReactDOM from "react-dom";
 
 document.addEventListener("DOMContentLoaded", () => {
     const root = document.getElementById("root");
-    ReactDOM.render(<h1>JBOPS: this is entry.jsx loading onto static_pages::root.html.erb</h1>, root);
+    ReactDOM.render(
+        <>
+        <h1>JBOPS</h1>
+        <h3>this is the static_pages::root.html.erb, rendering entry.jsx</h3> 
+        </>,
+         root);
 });
