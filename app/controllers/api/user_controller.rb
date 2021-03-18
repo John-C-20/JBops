a6fe143fe65f49ee78556268f9e84b5891268e09
John <johncheung1997@gmail.com>
2021-03-17 21:05:38 -0400

    Preparing project for production

---
+++ app/controllers/api/user_controller.rb
@@ -0,0 +1,17 @@
+class Api::UserController < ApplicationController
+    def create
+        @user = User.new(user_params)
+
+        if @user.save 
+            login!(@user) 
+            render :show 
+        else
+            render json: @user.errors.full_messages, status: 422 
+    end
+
+    private 
+    
+    def user_params
+        params.require(:user).permit(:username, :email, :password)
+    end
+end
---
+++ frontend/entry.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+document.addEventListener("DOMContentLoaded", () => {
+    const root = document.getElementById("root");
+    ReactDOM.render(<h1>JBOPS: this is entry.jsx loading onto static_pages::root.html.erb</h1>, root);
+});
