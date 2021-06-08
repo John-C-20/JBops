40695b8e30e77f90caccabd45f422f1869c89a36
John <johncheung1997@gmail.com>
2021-06-08 00:23:03 -0400

    fix small bug

--- app/controllers/api/searches_controller.rb
+++ app/controllers/api/searches_controller.rb
@@ -1,10 +1,21 @@
 class Api::SearchesController < ApplicationController
 
     def index
+        if params[:title] != ''
+    
         @playlists = Playlist.select('*').where("upper(playlists.playlist_name) LIKE upper('%#{params[:title]}%')") || []
         @songs = Song.select('*').where("upper(songs.song_title) LIKE upper('%#{params[:title]}%')") || []
         @albums = Album.select('*').where("upper(albums.album_title) LIKE upper('%#{params[:title]}%')") || []
         @artists = Artist.select('*').where("upper(artists.name) LIKE upper('%#{params[:title]}%')") || []
+
+        else 
+
+        @playlists = []
+        @songs = []
+        @albums = [] 
+        @artists = []
+
+        end
         render :index
     end
 end
